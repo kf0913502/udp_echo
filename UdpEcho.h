49de99444bab4d465508b57809bba2d731bb41e1
kariemfahmi <kfahmi@tcd.ie>
2022-10-18 18:35:27 +0300

    Added extern c to preserve name

--- UdpEcho.h
+++ UdpEcho.h
@@ -6,21 +6,45 @@
 #define UDP_ECHO_UDPECHO_H
 
 
-extern double getCurTimeInMs();
+int createSck();
+
+extern "C" int cCreateSck()
+{
+    return createSck();
+}
 
 
-extern int setSckTimeout(int fd, int timeoutSec);
+ double getCurTimeInMs();
 
-extern unsigned int createSck();
+extern "C" double cGetCurTimeInMs()
+{
+    return getCurTimeInMs();
+}
+
+
+ int setSckTimeout(int fd, int timeoutSec);
 
 //use 0 in ifaceIp to listen to all local interfaces
 extern int bindSck(int fd, int ifaceIp, short port);
 
+extern "C" double cBindSck(int fd, int ifaceIp, short port)
+{
+    return bindSck(fd,ifaceIp,port);
+}
 
-extern int sckWrite(int fd, int dstIp, short dstPort, char * data, int len);
+int sckWrite(int fd, int dstIp, short dstPort, char * data, int len);
+
+extern "C" int cSckWrite(int fd, int dstIp, short dstPort, char * data, int len)
+{
+    return sckWrite( fd,  dstIp,  dstPort, data,  len);
+}
 
 
-extern int sckRead(int fd, char * data, int len, int * srcIp, short * srcPort);
 
+ int sckRead(int fd, char * data, int len, int * srcIp, short * srcPort);
 
+extern "C" int cSckRead(int fd, char * data, int len, int * srcIp, short * srcPort)
+{
+    return sckRead( fd,  data,  len, srcIp,  srcPort);
+}
 #endif //UDP_ECHO_UDPECHO_H
